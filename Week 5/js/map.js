6174f5bc484d3d05ecb3ed9df83882abd8e20bef
jackwitherspoon <97418760+jackwitherspoon@users.noreply.github.com>
2022-04-25 14:36:36 -0700

    Merge branch 'main' of https://github.com/jackwitherspoon/DH151

---
+++ Week 5/js/map.js
@@ -0,0 +1,37 @@
+// Global variables
+let map;
+let lat = 0;
+let lon = 0;
+let zl = 3;
+let path = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv';
+let markers = L.featureGroup();
+
+
+
+// initialize
+$( document ).ready(function() {
+    createMap(lat,lon,zl);
+	readCSV(path);
+});
+
+// create the map
+function createMap(lat,lon,zl){
+	map = L.map('map').setView([lat,lon], zl);
+
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+}
+
+// function to read csv data
+function readCSV(path){
+	Papa.parse(path, {
+		header: true,
+		download: true,
+		complete: function(csvdata) {
+			console.log(csvdata);
+			
+		}
+	});
+}
+
